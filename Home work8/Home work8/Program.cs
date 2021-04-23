8b76bd6d14107dac8c5992af6c664204068dc3b3
TuralIslamli <turalislamli@gmail.com>
2021-04-23 12:49:49 +0400

    Home work8

--- Home work8/Home work8/Program.cs
+++ Home work8/Home work8/Program.cs
@@ -6,68 +6,33 @@ namespace Home_work8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Укажите длину массива.");
+            Console.WriteLine("Введите длину массива");
             int n = Convert.ToInt32(Console.ReadLine());
             int[] m = new int[n];
-            int[] a = new int[n / 2];
-            int[] b = new int[n / 2];
-            int[] c = new int[1];
-
-
+            int sort = 0;
             for (int i = 0; i < m.Length; i++)
+
             {
-                Console.WriteLine($"Укажите число {i + 1}");
+
+                Console.WriteLine($"Введите {i + 1} число");
                 m[i] = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"Число {i + 1} равно {m[i]}");
 
             }
-            int d = 0;
-            while (d == m.Length / 2 + 1)
+
+            int p = m.Length / 2 + m.Length % 2;
+
+            for (int j = 0; j < m.Length / 2; j++)
             {
-                c[0] = m[d];
+                sort = m[j];
+                m[j] = m[p + j];
+                m[p + j] = sort;
             }
 
-
-
-
-
-            if (n % 2 == 0)
+            Console.WriteLine("Вывод отсортированного массива");
+            for (int i = 0; i < m.Length; i++)
             {
-                int k = 0;
-                for (int j = 0; j < m.Length; j++)
-                {
-                    if (j < a.Length)
-                    {
-                        a[j] = m[j];
-                    }
-                    else
-                    {
-                        b[k] = m[j];
-                        k++;
-                    }
-                }
-                foreach (int pums in b) { Console.WriteLine(pums); }
-                foreach (int nums in a) { Console.WriteLine(nums); }
-            }
-            else
-            {
-                int k = 0;
-                for (int j = 0; j < m.Length - 1; j++)
-                {
-                    if (j < a.Length)
-                    {
-                        a[j] = m[j];
-                    }
-                    else
-                    {
-                        b[k] = m[j];
-                        c[k / 2] = m[j / 2 + 1];
-                        k++;
-                    }
-                }
-                foreach (int pums in b) { Console.WriteLine(pums); }
-                foreach (int dums in c) { Console.WriteLine(dums); }
-                foreach (int nums in a) { Console.WriteLine(nums); }
+                Console.WriteLine(m[i]);
             }
             Console.ReadKey();
 
